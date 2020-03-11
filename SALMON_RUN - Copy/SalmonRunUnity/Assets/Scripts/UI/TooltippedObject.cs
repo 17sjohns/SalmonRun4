27ceb50eece44e88d96438a3dfde767639183bb5
17sjohns <61259988+17sjohns@users.noreply.github.com>
2020-03-11 16:15:33 -0400

    Added the project to my repository

---
+++ SALMON_RUN - Copy/SalmonRunUnity/Assets/Scripts/UI/TooltippedObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/**
+ * Script for objects that will generate tooltips when the mouse is over them
+ */
+public class TooltippedObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // content that the tooltip will have
+    public string content;
+    public string cantBuy;
+
+    /**
+     * Handle mouse entering an object
+     */
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        bool canAfford = ManagerIndex.MI.MoneyManager.CanAfford(DragAndDropIcon.towerCost);
+        Tooltip.ShowTooltipInstance(canAfford ? content : cantBuy);
+    }
+
+    /**
+     * Handle mouse exiting the object
+     */
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        Tooltip.HideTooltipInstance();
+    }
+}
