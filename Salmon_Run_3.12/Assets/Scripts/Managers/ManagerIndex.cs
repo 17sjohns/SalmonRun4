3184b3029d585b316fbd833e59a03728acfbb9f9
17sjohns <61259988+17sjohns@users.noreply.github.com>
2020-03-12 16:45:20 -0400

    made a copy of SR
    Money and costs are working in this version

---
+++ Salmon_Run_3.12/Assets/Scripts/Managers/ManagerIndex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerIndex : MonoBehaviour
+{
+    public static ManagerIndex MI;
+
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private ColorManager colorManager;
+    [SerializeField] private TimeManager timeManager;
+    [SerializeField] private MoneyManager moneyManager;
+
+    public GameManager GameManager => gameManager;
+
+    public ColorManager ColorManager => colorManager;
+
+    public TimeManager TimeManager => timeManager;
+
+    public MoneyManager MoneyManager => moneyManager;
+
+    private void Awake()
+    {
+        if (!MI) MI = this;
+        else Destroy(gameObject);
+    }
+}
---
+++ Salmon_Run_3.12/Assets/Scripts/UI/LeftTopBarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftTopBarController : MonoBehaviour
+{
+    // turn counter text
+    public TextMeshProUGUI turnCounterText;
+
+    // coin text
+    public TextMeshProUGUI coinText;
+
+    /**
+     * Initialization function
+     */
+    private void Awake()
+    {
+        GameEvents.onTurnUpdated.AddListener(UpdateTopBarUI);
+    }
+
+    /**
+     * Update the UI for the top bar
+     * 
+     * Implementation of function from IUpdatableUI interface
+     */
+    private void UpdateTopBarUI()
+    {
+        turnCounterText.text = "Turn: " + GameManager.Instance.Turn;
+    }
+}
